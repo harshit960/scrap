6c94e37c93e3cb369050a9dff0c16f6cf37c477f
harshit960 <raj.harshit960@gmail.com>
2023-08-10 23:19:09 +0530

    updated for name

--- main.py
+++ main.py
@@ -47,8 +47,9 @@ def out(listt):
 options = webdriver.ChromeOptions()
 # options.add_argument("--blink-settings=imagesEnabled=false")
 #setproxy(options)
+name=input("Enter name")
 options.add_argument('--disable-blink-features=AutomationControlled')
-userdatadir = 'C:/Users/rajha/AppData/Local/Google/Chrome/User Data'
+userdatadir = f'C:/Users/{name}/AppData/Local/Google/Chrome/User Data'
 options.add_argument(f"--user-data-dir={userdatadir}")
 options.add_argument("--window-size=920,1080")
 driver = uc.Chrome(options=options)
